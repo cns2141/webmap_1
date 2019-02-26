f4bf5f5491d4bfca970fa150fa64a8f8cdfc4b4a
cns2141 <cns2141@columbia.edu>
2019-02-26 18:35:05 -0500

    Edit 2 Map

--- map.js
+++ map.js
@@ -1,1 +1,12 @@
-console.log("Hello, world!")
+'use strict'        
+
+console.log('Loaded map.js')
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiY25zMjE0MSIsImEiOiJjanNtZW5veWowMHM5NDRtcnRzYm9lZDBzIn0.gMWIq38Xalt5MBIy2n-6bQ'
+
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10',
+    center: [-73.96024, 40.80877],
+    zoom: 1
+})
